f71e3974ad0a96d0f63447054abe0d81c9ca0ad4
OCastelle <34087362+OCastelle@users.noreply.github.com>
2017-12-01 15:39:01 -0800

    Update indexCaptain.js

--- indexCaptain.js
+++ indexCaptain.js
@@ -1,19 +1,20 @@
 const Discord = require("discord.js");
 
-const client = new Discord.Client();
+const TOKEN = "Mzg0NTEzNjg4Mjk3NTM3NTM2.DPz6Rg.9XO3WJGvcs32Ra-y9mKNahF8XxU";
 
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
   console.log("Ready");
+bot.user.setPresence({ game: { name: "!+=Music=+!" , type: 0 } });
 });
 
 bot.on("message", function(message) {
   if (message.author.equals(bot.user)) return;
 
-  if (message.content == "Hi!"){
-      message.channel.sendMessage("Greetings!");
+  if (message.content == "XD!"){
+      message.channel.sendMessage("LOL!");
   }
 });
 
-client.login(process.env.BOT_TOKEN);
+bot.login(TOKEN);
